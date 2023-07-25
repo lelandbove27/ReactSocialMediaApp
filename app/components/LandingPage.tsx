8b69feaba9d28574914f13452935e9e0f23f6382
lbove27 <lbove@ucsd.edu>
2023-07-25 11:13:33 -0700

    first commit

---
+++ app/components/LandingPage.tsx
@@ -0,0 +1,33 @@
+import Image from 'next/image'
+
+function LandingPage() {
+  return (
+    <main className="overflow-hidden" id="landing-main">
+      <div className="flex-column container">
+        <header>
+          <h1 className="main-title">Social Media Site</h1>
+          <h2 className="secondary-title">Share your favorite images with your friends!</h2>
+        </header>
+        <section id="login">
+          <h3 className="secondary-title">Login</h3>
+          <form id="login-form" className="flex-column">
+            <div className="form-item">
+              <label htmlFor="username">Username: </label>
+              <input type="text" id="username" name="username"></input>
+            </div>
+            <div className="form-item">
+              <label htmlFor="password">Password: </label>
+              <input type="password" id="password" name="password"></input>
+            </div>
+          </form>
+        </section>
+      </div>
+      <div className="landing-image" style={{maxHeight: '100%', maxWidth: '50%'}}>
+        <Image id="landing-image" src="/../public/pexels-marek-339379.jpg" alt="camera" width={100} height={50} objectFit="contain" layout="responsive"/>
+      </div>
+    </main>
+  )
+}
+
+export default LandingPage
+
